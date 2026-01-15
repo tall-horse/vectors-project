6c28fb7809d05b8e51e864a70f435edbc34fbd62
tall-horse <dima.gamedev@pm.me>
2026-01-15 15:53:18 +0100

    better speed manipulation

--- My project/Assets/Scripts/Drive.cs
+++ My project/Assets/Scripts/Drive.cs
@@ -4,14 +4,13 @@ using System.Collections;
 
 public class Drive : MonoBehaviour
 {
-    
-    Vector2 dir = new Vector3(1f, 1f);
+    [SerializeField] private float speed = 1f;
     void Update()
     {
         float verticalInput = Input.GetAxis("Vertical");
         float horizonalInput = Input.GetAxis("Horizontal");
         Vector3 position = transform.position;
-        Vector3 appendPosition = new Vector3(horizonalInput, verticalInput, 0).normalized;
+        Vector3 appendPosition = new Vector3(horizonalInput, verticalInput, 0).normalized * speed;
         position += appendPosition * Time.deltaTime;
         Debug.Log(appendPosition);
         transform.position = position;
