1e124fcfa1769e4f7c88f8f6f4ae977ceed9b341
tall-horse <dima.gamedev@pm.me>
2026-01-15 15:37:18 +0100

    introduction to vectors

--- My project/Assets/Scripts/Drive.cs
+++ My project/Assets/Scripts/Drive.cs
@@ -1,29 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// A very simplistic car driving on the x-z plane.
 
 public class Drive : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
-
+    
+    Vector2 dir = new Vector3(1f, 1f);
     void Update()
     {
-        // Get the horizontal and vertical axis.
-        // By default they are mapped to the arrow keys.
-        // The value is in the range -1 to 1
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
-        // Move translation along the object's z-axis
-        transform.Translate(0, translation, 0);
-
-        // Rotate around our y-axis
-        transform.Rotate(0, 0, -rotation);
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizonalInput = Input.GetAxis("Horizontal");
+        Vector3 position = transform.position;
+        Vector3 appendPosition = new Vector3(horizonalInput, verticalInput, 0).normalized;
+        position += appendPosition * Time.deltaTime;
+        Debug.Log(appendPosition);
+        transform.position = position;
     }
 }
