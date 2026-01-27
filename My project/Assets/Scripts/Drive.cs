219ee363a0a7f909e109744a0d990aa7eb0740f8
tall-horse <dima.gamedev@pm.me>
2026-01-27 12:44:57 +0100

    normalized movement to point

--- My project/Assets/Scripts/Drive.cs
+++ My project/Assets/Scripts/Drive.cs
@@ -4,15 +4,19 @@ using System.Collections;
 
 public class Drive : MonoBehaviour
 {
-    [SerializeField] private float speed = 1f;
+    [SerializeField] private float speed = 0.01f;
+    [SerializeField] private GameObject fuelTank;
+    [SerializeField] private float stoppingDistance = 0.1f;
+    Vector3 direction;
+    void Start()
+    {
+        direction = fuelTank.transform.position - transform.position;
+    }
     void Update()
     {
-        float verticalInput = Input.GetAxis("Vertical");
-        float horizonalInput = Input.GetAxis("Horizontal");
-        Vector3 position = transform.position;
-        Vector3 appendPosition = new Vector3(horizonalInput, verticalInput, 0).normalized * speed;
-        position += appendPosition * Time.deltaTime;
-        Debug.Log(appendPosition);
-        transform.position = position;
+        if(Vector3.Distance(transform.position, fuelTank.transform.position) > stoppingDistance)
+        {
+            transform.position += direction.normalized * speed * Time.deltaTime;
+        }
     }
 }
